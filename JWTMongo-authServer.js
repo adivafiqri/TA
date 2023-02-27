79677f8cf472120f8df4051356ec2284d585aaa4
adivafiqri <adivafiqri@gmail.com>
2023-02-27 14:08:09 +0700

    token sudah bisa

--- JWTMongo-authServer.js
+++ JWTMongo-authServer.js
@@ -12,22 +12,31 @@ const dbPromise = connectToMongo();
 
 //refreshtoken
 let refreshTokensCollection;
+let userCollection;
 dbPromise.then((db) => {
   refreshTokensCollection = db.collection(
     process.env.REFRESH_TOKENS_COLLECTION_NAME
   );
+  userCollection = db.collection(process.env.USER_COLLECTION_NAME);
 });
 
-app.post("/token", (req, res) => {
+app.post("/token", async (req, res) => {
   const refreshToken = req.body.token;
+
   if (refreshToken == null) return res.sendStatus(401);
-  refreshTokensCollection.findOne({ token: refreshToken }, (err, result) => {
-    if (err || result == null) return res.sendStatus(403);
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-      if (err) return res.sendStatus(403);
-      const accessToken = generateAccessToken({ name: user.name });
-      res.json({ accessToken: accessToken });
-    });
+  const findrefreshTokens = await refreshTokensCollection.findOne({
+    token: refreshToken,
+  });
+
+  if (!findrefreshTokens) {
+    console.log("tidak ada collection");
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.username });
+    res.json({ accessToken: accessToken });
   });
 });
 
@@ -39,50 +48,45 @@ app.delete("/logout", (req, res) => {
   res.sendStatus(204);
 });
 
-dbPromise.then((db) => {
-  app.post("/login", async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-    // Authenticate user in MongoDB
-    const collection = db.collection(process.env.MONGO_COLLECTION_NAME);
-    const user = await collection.findOne({
-      username: username,
-      password: password,
-    });
+  // Authenticate user in MongoDB
 
-    if (!user) {
-      return res.sendStatus(401);
-    }
-
-    // Generate tokens and respond to client
-    const accessToken = generateAccessToken({ name: username });
-    const refreshToken = generateRefreshToken({ name: username });
-
-    //memasukan refresh token ke collection
-    refreshTokensCollection.insertOne(
-      { token: refreshToken },
-      (err, result) => {
-        if (err) return res.sendStatus(500);
-      }
-    );
-
-    //respons
-    res.json({ accessToken: accessToken, refreshToken: refreshToken });
+  const user = await userCollection.findOne({
+    username: username,
+    password: password,
   });
 
-  function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: "30s",
-      algorithm: "HS384",
-    });
+  if (!user) {
+    return res.sendStatus(401);
   }
 
-  function generateRefreshToken(user) {
-    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
-  }
+  // Generate tokens and respond to client
+  const accessToken = generateAccessToken({ name: username });
+  const refreshToken = generateRefreshToken({ name: username });
 
-  app.listen(4000, () => {
-    console.log("Server started on port 4000");
+  //memasukan refresh token ke collection
+  refreshTokensCollection.insertOne({ token: refreshToken }, (err, result) => {
+    if (err) return res.sendStatus(500);
   });
+
+  //respons
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "30s",
+    algorithm: "HS384",
+  });
+}
+
+function generateRefreshToken(user) {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+}
+
+app.listen(4000, () => {
+  console.log("Server started on port 4000");
 });
