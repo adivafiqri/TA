60036f0bba4a21937ffd3f08d529bb8879e4553c
adivafiqri <adivafiqri@gmail.com>
2023-03-03 13:56:19 +0700

    add docker jwt

---
+++ JWT/JWTMongo-server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
+const connectToMongo = require("./dbMongo");
+const dbPromise = connectToMongo();
+
+let postsCollection;
+dbPromise.then((db) => {
+  postsCollection = db.collection(process.env.POSTS_COLLECTION_NAME);
+});
+
+app.get("/posts", authenticateToken, async (req, res) => {
+  // Use postsCollection instead of posts
+  const posts = await postsCollection
+    .find({ username: req.user.name })
+    .toArray();
+  res.json(posts);
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.listen(3000, () => {
+  console.log("Server started oneee port 3000");
+});
---
+++ JWT/dbMongo.js
@@ -0,0 +1,21 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+async function connectToMongo() {
+  const uri =
+    "mongodb+srv://adivafiqri:m7K0G0eMK7qL27ag@taadiva.3lgefed.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    return client.db("pengujiantoken"); // return the database connection
+  } catch (e) {
+    console.error("Error connecting to MongoDB", e);
+  }
+}
+
+module.exports = connectToMongo;
